b26fa59fbcd3a4ce8dd1bdc6b51d08a7bcde9638
andresf2448 <58791994+andresf2448@users.noreply.github.com>
2021-06-06 23:38:23 -0700

    primera ruta del back lista

---
+++ PI-Dogs/api/src/routes/dogs.js
@@ -0,0 +1,47 @@
+require('dotenv').config();
+const { API_KEY } = process.env;
+const { Dog } = require('../db');
+const axios = require('axios');
+const {Op} = require('sequelize');
+const { Router } = require('express');
+const router = Router();
+
+router.get('/', async (req, res) => {
+
+    const {name} = req.query;
+
+    if(!name){
+        try {
+            let results = await axios.get(`https://api.thedogapi.com/v1/breeds?api_key=${API_KEY}`);
+
+            let eightResults = results.data.slice(0,8);
+            console.log("melo");
+            return res.send(eightResults);
+        }
+        catch (error){
+            res.send(error);
+        }
+    }
+
+    if(name){
+        try {
+            let resultsApi = await axios.get(`https://api.thedogapi.com/v1/breeds?api_key=${API_KEY}`);
+            
+            let resultsDb = await Dog.findAll({
+                where: {name:{
+                    [Op.iLike]: `%${name}%`
+                }}
+            });
+
+            let resultsApiDb = resultsDb.concat(resultsApi.data);
+            let results = resultsApiDb.slice(0,8);
+            
+            res.send(results);
+        }
+        catch (error){
+            res.send(error);
+        }
+    }
+});
+
+module.exports = router;
