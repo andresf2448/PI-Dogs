30f7038d4362ef04f37f9239ae2b0a7e729a4859
andresf2448 <58791994+andresf2448@users.noreply.github.com>
2021-06-20 23:28:36 -0700

    primer avance en css

--- PI-Dogs/client/src/Components/PrincipalPage.jsx
+++ PI-Dogs/client/src/Components/PrincipalPage.jsx
@@ -1,15 +1,19 @@
 import { Fragment } from 'react';
 import { Link } from 'react-router-dom';
+import '../Css/PrincipalPage.css';
 
 function PrincipalPage(){
     return (
-        <Fragment>
-            <h1>Henry Dogs</h1>
-            <div>Imagen principal</div>
-            <Link to="/principal">
-                <button>Buscar raza de perro</button>
-            </Link>                
-        </Fragment>
+        <div className="principal">
+            <div>
+                <h1 className="h1">Henry Dogs</h1>
+            </div>
+            <div>
+                <Link to="/principal">
+                    <button className="but">Buscar tu raza de perro</button>
+                </Link>
+            </div>            
+        </div>
     )
 }
 
